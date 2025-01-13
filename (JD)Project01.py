85f78c02e3b54977c5ff6d8b0370c13cceefa39a
GFS Fellow 6 <gfsfellow6@GFSs-MacBook-Air.local>
2025-01-13 13:12:57 -0500

    first commit

---
+++ (JD)Project01.py
@@ -0,0 +1,13 @@
+def main():
+	print("This program will ask you a series of questions like you name and education history")
+	name = input("What is your name?")
+	college_name = input("What college do you go to?")
+	highschool_name = input("What highschool did you go to?")
+	print(" Hello {}" .format(name))
+	print("You go to {}" .format(college_name))
+	print("But you went to highschool at {}" .format(highschool_name))
+	print("We want to know if you like programming!")
+
+
+if __name__ == "__main__":
+	main()  
