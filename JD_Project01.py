15a3e7ec1111089add13d05f0e8b4f21e59f3194
GFS Fellow 6 <gfsfellow6@GFSs-MacBook-Air.local>
2025-01-14 12:55:25 -0500

    added

--- JD_Project01.py
+++ JD_Project01.py
@@ -1,11 +1,14 @@
 def main():
-	print("This program will ask you a series of questions like you name and education history")
+	print("This program will ask you a series of questions like your name and education history")
 	name = input("What is your name?")
 	college_name = input("What college do you go to?")
 	highschool_name = input("What highschool did you go to?")
-	print(" Hello {}" .format(name))
-	print("You go to {}" .format(college_name))
-	print("But you went to highschool at {}" .format(highschool_name))
+	print(" Hello {}, so you went to highschool at {} and you go to college at {}." .format(name, highschool_name, college_name))
+	print("Thank you for answering my questions!")
+	institution_morefun = input("Hey! one last question, which institution you think is the most fun?")
+	institution_lessfun = input("which institution do you think is the least fun?")
+	print("So... you think {} is more fun than {}" .format(institution_morefun, institution_lessfun))
+	print("Thank you so much for your time. Goodbye!")
 	
 
 
